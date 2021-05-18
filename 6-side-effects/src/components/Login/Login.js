02094b8890cdd4ef3697d4bf6c8a10a56a854cc4
Ethan Thompson <ecthompson99@gmail.com>
2021-05-17 19:22:28 -0700

    [6-side-effects] Added side effects. Added component mount side effect in App.js to keep user logged in on refresh, also added useReducer hooks for cleaner state management and validation

---
+++ 6-side-effects/src/components/Login/Login.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState, useReducer } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const emailReducer = (prevState, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') }
+  }
+  if (action.type === 'BLUR') {
+    return { value: prevState.value, isValid: prevState.value.includes('@') }
+  }
+  return initialEmailState;
+}
+
+const passwordReducer = (prevState, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.trim().length > 6 }
+  }
+  if (action.type === 'BLUR') {
+    return { value: prevState.value, isValid: prevState.value.trim().length > 6 }
+  }
+  return initialPasswordState;
+}
+
+const initialEmailState = { value: '', isValid: false };
+const initialPasswordState = { value: '', isValid: false };
+
+const Login = (props) => {
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, initialEmailState);
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, initialPasswordState);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      console.log('running validation!')
+      setFormIsValid(
+        emailState.value.includes('@') && passwordState.value.trim().length > 6
+      );
+    }, 500); // debounce for 500 milliseconds.
+
+    return () => {
+      console.log('running cleanup!')
+      clearTimeout(timerId);
+    };
+
+  }, [emailState.value, passwordState.value]);
+
+  const emailChangeHandler = (event) => {
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
+  };
+
+  const passwordChangeHandler = (event) => {
+    dispatchPassword({ type: 'USER_INPUT', val: event.target.value })
+  };
+
+  const validateEmailHandler = () => {
+    dispatchEmail({ type: 'BLUR' })
+  };
+
+  const validatePasswordHandler = () => {
+    dispatchPassword({ type: 'BLUR' })
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, passwordState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passwordState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
